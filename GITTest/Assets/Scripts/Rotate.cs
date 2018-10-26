69798f6ad9d74ea8ff26844d204b0f976062f75e
Brynli <brynli@Brynlis-iMac.home>
2018-10-26 14:39:06 +0100

    initial load

---
+++ GITTest/Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float rotatingSpeed = 5.0f;
+
+	
+	void Update () 
+    {
+        this.transform.Rotate(Vector3.up * Time.deltaTime * rotatingSpeed);
+        this.transform.Rotate(Vector3.right * Time.deltaTime * rotatingSpeed);
+    }
+}
