28a03edddc3c3e833efa79cce18f8da6aca7e04b
brynli <brynli@yahoo.com>
2018-10-26 16:10:47 +0100

    Added initial startup message - Feature 1

--- GITTest/Assets/Scripts/Rotate.cs
+++ GITTest/Assets/Scripts/Rotate.cs
@@ -6,8 +6,12 @@ public class Rotate : MonoBehaviour {
 
     public float rotatingSpeed = 5.0f;
 
-	
-	void Update () 
+    private void Start()
+    {
+        Debug.Log("This is a new startup message !");
+    }
+
+    void Update () 
     {
         this.transform.Rotate(Vector3.up * Time.deltaTime * rotatingSpeed); // This is a comment from GitHub
         this.transform.Rotate(Vector3.right * Time.deltaTime * rotatingSpeed); // Second Comment from GitHub
